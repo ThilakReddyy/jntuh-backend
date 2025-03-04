7c20b5e0342893f27bbaebac2a4c2895e2eccd26
thilakreddyy <thilakreddy19102000@gmail.com>
2025-03-04 11:25:49 +0530

    Add: logger

--- messaging/consumer.py
+++ messaging/consumer.py
@@ -7,33 +7,36 @@ from database.operations import save_to_database
 from scrapers.resultNotificationScraper import get_notifications
 from scrapers.resultScraper import ResultScraper
 from scrapers.serverChecker import check_url
-from utils.logger import rabbitmq_logger, logger
+from utils.logger import rabbitmq_logger, logger, scraping_logger
 
 
 # Define a function to process messages
 async def process_message(message_body: str):
-    """
-    Process the consumed message.
-    Replace this logic with your custom processing code.
-    """
-    rabbitmq_logger.info(f"Processing message: {message_body}")
+    try:
+        """
+        Process the consumed message.
+        Replace this logic with your custom processing code.
+        """
+        rabbitmq_logger.info(f"Processing message: {message_body}")
 
-    # Remove the roll number from Redis after successful processing
-    if redisConnection.client:
-        redisConnection.client.srem("rabbitmq_roll_numbers", message_body)
-        rabbitmq_logger.info(f"Removed roll number {message_body} from Redis.")
+        # Remove the roll number from Redis after successful processing
+        if redisConnection.client:
+            redisConnection.client.srem("rabbitmq_roll_numbers", message_body)
+            rabbitmq_logger.info(f"Removed roll number {message_body} from Redis.")
 
-    url = check_url()
-    if not url:
-        return
+        url = check_url()
+        if not url:
+            return
 
-    scraper = ResultScraper(message_body, url)
-    results = await scraper.run()
-    if results is None:
-        logger.warning(f"Failed to get results: {message_body}")
-        return
-    logger.info(f"Results was successfully extracted: {message_body}")
-    await save_to_database(results)
+        scraper = ResultScraper(message_body, url)
+        results = await scraper.run()
+        if results is None:
+            logger.warning(f"Failed to get results: {message_body}")
+            return
+        logger.info(f"Results was successfully extracted: {message_body}")
+        await save_to_database(results)
+    except Exception as e:
+        scraping_logger.error(f"Error while scarping results: {e}")
 
 
 async def consume_messages(app: FastAPI):
